cb48f9aa2e181834e7f5dc1d101e09850f6ccc60
Pop2323 <omar4500455@gmail.com>
2023-06-15 10:25:11 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Title: This code will print using the puts function
+ *
+ * Return: Return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
