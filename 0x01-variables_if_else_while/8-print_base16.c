f73f949b42ab152c0854d5ada159fdd7a0bd3772
Pop2323 <omar4500455@gmail.com>
2023-06-16 16:02:30 -0700

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Title: a program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	}
+		putchar('0' + num);
+	}
+	for (num = 0; num < 6; num++)
+	{
+		putchar('a' + num);
+	}
+	putchar('\n');
+
+	return (0);
+}
