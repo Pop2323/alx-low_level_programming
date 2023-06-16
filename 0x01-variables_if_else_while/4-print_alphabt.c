6ebfd7306dc07c790d528768800c1d78d20d98c9
Pop2323 <omar4500455@gmail.com>
2023-06-16 15:18:05 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Title: a C program that prints the alphabet in lowercase, except q and e
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+		if (alph != 'q' && alph != 'e')
+		putchar(alph);
+}
+	putchar('\n');
+
+	return (0);
+}
