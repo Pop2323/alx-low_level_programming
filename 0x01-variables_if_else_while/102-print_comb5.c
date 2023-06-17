33823571374d7247715ca22981af069c61590a8a
Pop2323 <omar4500455@gmail.com>
2023-06-16 19:00:22 -0700

    print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,10 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 98; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			printf("%02d %02d", num1, num2);
-			if (num1 != 99 || num2 != 98)
+			if (num1 != 98 || num2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
