bebe1a1b29631da72c2707bb0f0595e14a888492
Pop2323 <omar4500455@gmail.com>
2023-06-20 18:07:59 -0700

    100_times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table -  function that prints the n times table, starting with 0
+ *
+ * @n: compare with other parameter
+*/
+
+void print_times_table(int n)
+{
+	int i, j, sum;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; ++i)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; ++j)
+			{
+				_putchar('.');
+				_putchar(' ');
+
+				sum = i * j;
+
+				if (sum <= 9 && sum <= 99)
+					_putchar(' ');
+				if (sum >= 100)
+				{
+					_putchar((sum / 10) + '0');
+					_putchar((sum % 10) + '0');
+				} else if (sum >= 10 && sum <= 99)
+					_putchar((sum / 10) + '0');
+				_putchar((sum % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
