60912e174d5fe56769dae5024fd2ba55a052a7ec
Pop2323 <omar4500455@gmail.com>
2023-06-22 13:38:50 -0700

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,15 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (int i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (int j = 1; j <= i; j++)
+			for (j = 1; j <= i; j++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
