d75347b0583db3651dfd8556f53d342e80df3b2e
Pop2323 <omar4500455@gmail.com>
2023-06-22 13:04:18 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,18 @@ void more_numbers(void)
 {
 	int i, j, n;
 
-	for (j = 1; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			n = i;
 			if (i > 9)
 			{
-				_putchar(1 + 48);
+				_putchar('1');
 				n = i % 10;
 			}
+			_putchar(n + '0');
 		}
-		_putchar(n + 48);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
