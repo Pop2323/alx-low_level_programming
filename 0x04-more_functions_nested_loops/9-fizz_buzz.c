77768ad67b9f6cff042721876663bea67b5d454e
Pop2323 <omar4500455@gmail.com>
2023-06-22 14:23:44 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-	if (n % 3 == 0 && !(n % 5 == 0))
+	if (n % 3 == 0)
 		printf("Fizz");
-	else if (n % 5 == 0 && !(n % 3 == 0))
+	else if (n % 5 == 0)
 		printf("Buzz");
 	else if (n % 3 == 0 && n % 5 == 0)
 		printf("FizzBuzz");
 	else
-		print("%d", n);
+		printf("%d", n);
 
 	if (n != 100)
-		printf(' ');
+		printf(" ");
 	else
 		printf("\n");
 	}
