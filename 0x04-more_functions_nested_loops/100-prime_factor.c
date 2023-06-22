ce36d3851852df256399cad4ee448b17066ad23b
Pop2323 <omar4500455@gmail.com>
2023-06-22 16:01:10 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**prime_factor - program that finds and prints the largest prime factor
+ *
+ * @x: input num
+ *
+ * Return: return 0
+*/
+
+unsigned long long largest_prime_factor(unsigned long long n)
+{
+	unsigned long long factor = 2;
+	
+	while (factor * factor <= n)
+	{
+		if (n % factor == 0)
+		{
+			n /= factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	return n;
+
+
+int main()
+{
+	unsigned long long number = 612852475143;
+	unsigned long long largest_factor = largest_prime_factor(number);
+	
+	printf("The largest prime factor of %llu is %llu\n", number, largest_factor);
+	
+	return 0;
+}
