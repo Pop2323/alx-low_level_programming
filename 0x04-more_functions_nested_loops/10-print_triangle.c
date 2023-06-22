50e583e1af2c4e439bfcc0998130a40908e06d69
Pop2323 <omar4500455@gmail.com>
2023-06-22 14:37:13 -0700

    print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle -  function that prints a triangle
+ *
+ * @size: is the size of the triangle
+ *
+ * Return: return 0
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar("\n");
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if ((i + j) <= size)
+					_putchar(" ");
+				else
+					-putchar("#");
+			}
+			_putchar('\n');
+		}
+
+	}
+}
