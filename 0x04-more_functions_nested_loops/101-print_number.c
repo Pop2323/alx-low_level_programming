644614fb9a5fb4b806f7038bb42a5b575cfd9388
Pop2323 <omar4500455@gmail.com>
2023-06-22 15:36:06 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: int nunber
+ *
+ * Return: return 0
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + (n % 10));
+}
