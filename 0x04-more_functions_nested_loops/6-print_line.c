d9643f6f0be076e48504b52ad7342d23f6875956
Pop2323 <omar4500455@gmail.com>
2023-06-22 13:24:16 -0700

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,13 @@
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (int i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
