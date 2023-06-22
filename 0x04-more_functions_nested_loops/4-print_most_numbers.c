fd62440222cf99ee589216c00ad71cc6db359f58
Pop2323 <omar4500455@gmail.com>
2023-06-22 16:24:26 -0700

    print_number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 || num == 4)
+		if (num == 2 && num == 4)
 			continue;
 		_putchar(48 + num);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
