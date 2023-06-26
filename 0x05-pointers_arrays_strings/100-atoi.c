9c2c447b4c86d1b228e5b0ae53189ce573b64c9a
Pop2323 <omar4500455@gmail.com>
2023-06-26 15:33:05 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,29 +10,17 @@
 
 int _atoi(char *s)
 {
+	unsigned int num = 0;
 	int sign = 1;
-	int num = 0;
-	int i = 0;
 
-	while (s[i] == ' ')
+	for (; *s != '\0'; s++)
 	{
-		i++;
-	}
-
-		if (s[i] == '-')
-		{
-			sign = -1;
-			i++;
-		}
-		else if (s[i] == '+')
-		{
-			i++;
-		}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
 	}
 
 	return (num * sign);
