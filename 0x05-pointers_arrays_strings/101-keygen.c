b1a9fe145c19b3127b308b70c477c6f4d8580816
Pop2323 <omar4500455@gmail.com>
2023-06-26 15:53:25 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  generates random valid passwords for the program 101-crackme
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int calc;
+	char str;
+
+	srand(time(NULL));
+	while (calc <= 2654)
+	{
+		calc = calc + str;
+		putchar(str)
+	}
+	putchar(2772 - calc);
+	return (0);
+}
