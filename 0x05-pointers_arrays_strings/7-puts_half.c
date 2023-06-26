503a12934a5e71f5f4ec8b655b950d1a0daec4fe
Pop2323 <omar4500455@gmail.com>
2023-06-26 14:13:17 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * @str: char str
+ *
+ * Return: Nothing
+*/
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+	int start_index;
+
+	if (len % 2 == 0)
+	{
+		start_index = len / 2;
+	}
+	else
+	{
+		start_index = (len - 1) / 2;
+	}
+	for (i = start_index; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
