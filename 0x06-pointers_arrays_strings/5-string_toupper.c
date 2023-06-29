418c531e2e3ebdc2674a66a1797d19f6ea579e9d
Pop2323 <omar4500455@gmail.com>
2023-06-29 09:53:18 -0700

    string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,13 @@
 
 char *string_toupper(char *str)
 {
-	for (int i = 0; str[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] - 'a' + 'A';
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
