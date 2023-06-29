a3d00c620b1214c2c844a58abc94a60221f2c617
Pop2323 <omar4500455@gmail.com>
2023-06-29 08:57:34 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,13 @@ char *_strncat(char *dest, const char *src, int n)
 	int len, i;
 
 	len = 0;
+
 	while (dest[len])
 		len++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[len + i] = src[i];
+	dest[len + i] = '\0';
 
 	return (dest);
 }
