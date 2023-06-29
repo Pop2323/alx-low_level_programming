cc38ed5bdcd1015dc90e798a74ad64ee6627f270
Pop2323 <omar4500455@gmail.com>
2023-06-29 14:05:40 -0700

    103-infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * *infinite_add - function that adds two numbers
@@ -9,50 +10,37 @@
  * Return: calc
 */
 
-char *reverse_string(char *str)
-{
-	int length = strlen(str);
-	int i, j;
-
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		char temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-	}
-	return str;
-}
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
 
 	if (len1 + len2 >= size_r)
-	{
-		return (NULL);
-	}
-
+		return (0);
 	int carry = 0;
 	int i = len1 - 1;
 	int j = len2 - 1;
-	int k = 0;
+	int k = size_r - 1;
 
-	while (i >= 0 || j >= 0 || carry > 0)
+	while (i >= 0 || j >= 0)
 	{
-		int digit1 = (i >= 0) ? (n1[i] - '0') : 0;
-		int digit2 = (j >= 0) ? (n2[j] - '0') : 0;
+		int sum = carry;
 
-		int sum = digit1 + digit2 + carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 		carry = sum / 10;
-		r[k] = (sum % 10) + '0';
-		
+		r[k] = sum % 10 + '0';
 		i--;
 		j--;
-		k++;
+		k--;
 	}
-	r[k] = '\0';
-	reverse_string(r);
-
-	return r;
+	if (carry > 0)
+	{
+		if (k < 0)
+			return (0);
+		r[k] = carry + '0';
+	}
+	return (&r[k]);
 }
