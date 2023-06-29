1decdc3dfba879d9ac697782aed10fb14d113e8f
Pop2323 <omar4500455@gmail.com>
2023-06-29 12:52:58 -0700

    103-infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,31 +12,23 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	int j = 0;
-	int len = 0;
-	int c = 0;
-	int v, s, m;
+	int i = 0; j = 0; len = 0; c = 0; v, s, m;
 
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
 		j++;
-
 	if (i > j)
 		len = i;
 	else
 		len = j;
-
-	if (len + 1 >= size_r)
+	if (len + 1 > size_r)
 		return (0);
 	r[len] = '\0';
-
 	for (v = len - 1; v >= 0; v--)
 	{
 		i--;
 		j--;
-
 		if (i >= 0)
 			m = n1[i] - '0';
 		else
@@ -45,19 +37,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			s = n2[j] - '0';
 		else
 			s = 0;
-
 		r[v] = (m + s + c) % 10 + '0';
 		c = (m + s + c) / 10;
 	}
-
 	if (c == 0)
 	{
 		r[len + 1] = '\0';
-
 		if (len + 2 > size_r)
 			return (0);
-
-		while (len-- > 0)
+		while (len-- >= 0)
 			r[len + 1] = r[len];
 		r[0] = c + '0';
 	}
