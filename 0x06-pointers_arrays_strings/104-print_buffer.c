c7e7a1f595dfc4bdf621253f42301d04ddf7c3b7
Pop2323 <omar4500455@gmail.com>
2023-07-03 09:03:54 -0700

    104-print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,27 +12,23 @@
 void print_buffer(char *b, int size)
 {
 	if (size <= 0)
+	{
 		printf("\n");
 		return;
+	}
 	int i, j;
 
 	for (i = 0; i < size; i += 10)
+	{
 		printf("%08x: ", i);
-		for (j = 0; j < 10; j += 2)
-			if (i + j < size)
-				printf("%02x%02x ", (unsigned char)b[i + j], (unsigned char)b[i + j + 1]);
-			else
-				printf("     ");
-		printf(" ");
-		for (j = 0; j < 10; j++)
-			if (i + j < size)
-				char c = b[i + j];
-
-				if (isprint(c))
-					printf("%c", c);
-				else
-					printf(".");
-			else
-				printf(" ");
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			printf("%02x%s", (unsigned char)b[j], (j % 2 == 1) ? " " : "");
+		}
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			printf("%c", (b[j] >= 32 && b[j] <= 126) ? b[j] : '.');
+		}
 		printf("\n");
+	}
 }
