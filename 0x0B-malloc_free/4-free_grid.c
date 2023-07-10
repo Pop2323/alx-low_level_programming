2b5a6d91538b4f0499a91e984f2b5780eaebbef9
Pop2323 <omar4500455@gmail.com>
2023-07-10 12:55:42 -0700

    free_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * free_grid - func that frees a 2 dimensional grid created by your alloc_grid
+ *
+ * @grid: ptr to ptr int value
+ *
+ * @height: int vale
+ *
+ * Return: return Null or value
+*/
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL)
+	{
+		return;
+	}
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
