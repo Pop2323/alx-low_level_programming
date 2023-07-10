aceff338da665c1a1653997f3707eea9faa0935c
Pop2323 <omar4500455@gmail.com>
2023-07-10 14:07:29 -0700

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -40,22 +40,3 @@ int **alloc_grid(int width, int height)
 	}
 	return (ptr);
 }
-/**
- * free_grid - func that take 2 parameter
- * @grid: ptr to ptr
- * @height: integer
-*/
-void free_grid(int **grid, int height)
-{
-	if (grid == NULL || height <= 0)
-	{
-		return;
-	}
-	int i;
-	
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
-	}
-	free(grid);
-}
