0db4ef3d2713eeab6fb3b736b21b6b32fbecfefa
Pop2323 <omar4500455@gmail.com>
2023-07-13 09:49:14 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,5 +36,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	min_size = (old_size < new_size) ? old_size : new_size;
 	memcpy(new_ptr, ptr, min_size);
 	free(ptr);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
 	return (new_ptr);
 }
