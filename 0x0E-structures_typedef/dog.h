695361f091d620b2af6dc4e1f6ed23d9ffb8d283
Pop2323 <omar4500455@gmail.com>
2023-07-17 12:51:57 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,12 @@
 
 /**
  * struct dog - new struct type named dog
- * 
+ *
  * @name: ptr to a char representing the name of the dog
  * @age: floating-point var representing the age of the dog
  * @owner: ptr to a char representing the owner's name of the dog
- * 
+ *
+ * Description: dog information name, age and owner
 */
 
 struct dog
@@ -16,12 +17,12 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-*/
+
 #endif
