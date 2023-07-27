122e051c9de322e1c7e6a68bd72d0c366519bb4c
Pop2323 <omar4500455@gmail.com>
2023-07-27 10:19:10 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,8 @@ section       .text
         extern printf
         global main
 main:
-        mov              edi.  msg
-        mov              eax.  0
+        mov              edi,  msg
+        mov              eax,  0
         call     printf
 
 
