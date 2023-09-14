a883435cd64e2ad73705c2d5ae1f60d04e64d1b7
Pop2323 <omar4500455@gmail.com>
2023-09-14 11:07:18 -0700

    3-add_dnodeint_end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* add_dnodeint_end - func that adds a new node at the end of a dlistint_t list.
+*
+* @head: head of the list
+* @n: the elm value
+*
+* Return: return the addr of the elm.
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	h = *head;
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next;
+	}
+	else
+	{
+		*head = new;
+	}
+	new->prev = h;
+	return (new);
+}
