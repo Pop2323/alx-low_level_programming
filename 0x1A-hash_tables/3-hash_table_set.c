2357ea8efa05f2b24841a4970d7dca6153c7799f
Pop2323 <omar4500455@gmail.com>
2023-10-10 16:25:55 -0700

    3-hash_table commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,19 +14,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-	size - ht->size;
+	size = ht->size;
 	index = key_index((const unsigned char *)key, size);
 
 	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
-		return (1);
-		new_node = malloc(sizeof(hash_node_t));
-		if (new_node == NULL)
-			return (0);
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		new_node->next = ht->array[index];
-		ht->array[index] =  new_node;
-		return (1);
+		return (1);	
+	}
+	new_node = malloc(sizeof(hash_node_t);
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
