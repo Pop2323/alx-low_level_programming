c4a645fa839939109f052847553d34312a6d4f3e
Pop2323 <omar4500455@gmail.com>
2023-11-22 12:42:54 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """returns the perimeter of the island described in grid:"""
+
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     res = 0
@@ -12,13 +14,3 @@ def island_perimeter(grid):
                 if i > 0 and grid[i][j - 1] == 1:
                     res -= 2
     return res
-
-
-# grid = [
-#         [0, 0, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 0],
-#         [0, 1, 1, 1, 0, 0],
-#         [0, 0, 0, 0, 0, 0]
-#     ]
-# print(island_perimeter(grid))
