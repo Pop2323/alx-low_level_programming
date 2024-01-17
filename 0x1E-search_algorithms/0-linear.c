ba3df2ccb52c509a5811c0f09f6ce869e62ecd5d
Pop2323 <omar4500455@gmail.com>
2024-01-17 10:46:16 -0800

    search-algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 
-
 /**
  * linear_search - searches for a value in an array of integers
- * 
+ *
  * @array: pointer to the first element of the array
  * @size: number of elements in the array
  * @value: value to search for
@@ -15,19 +14,19 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    if (array == NULL)
-    {
-        return -1;
-    }
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%u] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_search - searches for a value in an array of integers
- * 
+ *
  * @array: pointer to the first element of the array
  * @size: number of elements in the array
  * @value: value to search for
@@ -11,37 +11,37 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int right, mid, left;
-    int i;
+	int right, mid, left;
+	int i;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
 
-    left = 0;
-    right = size - 1;
-    while (left <= right)
-    {
-        int mid = left + (right - left) / 2;
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            printf("%d ", array[i]);
-        }
-        printf("\n");
-        if (array[mid] == value)
-        {
-            return (mid);
-        }
-        else if (array[mid] <= value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
-    return (-1);
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] <= value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	return (-1);
 }
