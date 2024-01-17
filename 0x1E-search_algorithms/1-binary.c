861954bad05b18e4238cfd91cf8bf65b33b49095
Pop2323 <omar4500455@gmail.com>
2024-01-17 11:19:49 -0800

    search_algorithm update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,28 +20,25 @@ int binary_search(int *array, size_t size, int value)
 	}
 	left = 0;
 	right = size - 1;
+
 	while (left <= right)
 	{
-		mid = left + (right - left) / 2;
-
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
 		}
-		printf("\n");
+		mid = left + (right - left) / 2;
 		if (array[mid] == value)
-		{
 			return (mid);
-		}
-		else if (array[mid] <= value)
-		{
+		else if (array[mid] < value)
 			left = mid + 1;
-		}
 		else
-		{
 			right = mid - 1;
-		}
 	}
 	return (-1);
 }
